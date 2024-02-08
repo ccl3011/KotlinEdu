68a4db7f736d1ee3cd0f5772bf04f7fad8c7ef10
[티시스] 최승연 <emilychoi@tsis.co.kr>
2024-02-08 16:10:53 +0900

    06-3 정적 변수와 메서드

--- src/test/java/chap06/section03/Customer.java
+++ src/test/java/chap06/section03/Customer.java
@@ -1,5 +1,6 @@
 package chap06.section03;
-//자바의 Customer 클래스
+
+//자바의 chap06.section03.Customer 클래스
 public class Customer {
     public static final String LEVEL = "BASIC"; //static 필드
     public static void login() { //static 메서드
---
+++ src/test/kotlin/chap06/section03/KCustomer.kt
@@ -0,0 +1,9 @@
+package chap06.section03
+
+class KCustomer {
+    companion object{
+        const val LEVEL = "INTERMEDIATE"
+        @JvmStatic fun login() = println("Login...") //애노테이션 표기 사용
+    }
+}
+
